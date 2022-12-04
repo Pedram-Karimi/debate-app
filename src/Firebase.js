eca8dacc0c389fb4a932ca37850c486ee54fc3cd
Pedram <pedramkarimi1001@gmail.com>
2022-12-04 03:15:04 +0000

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from "@firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FB_API_KEY,
+  apiKey: "AIzaSyBqSXkegmjMNFWN3_PiST-OCs8c1azhzgs",
   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FB_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
